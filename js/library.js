9359ccb4ba07388935348007bc4f481297398a3f
Hung Le <dangky1410@gmail.com>
2021-02-01 18:39:14 -0600

    update version 1

---
+++ js/library.js
@@ -0,0 +1,18 @@
+"use strict";
+const books = document.getElementById("books");
+
+(async function start() {
+  const res = await fetch(
+    "https://elibraryrestapi.herokuapp.com/elibrary/api/book/list"
+  );
+  const results = await res.json();
+  console.log(results);
+
+  results.forEach((item) => {
+    books.innerHTML += render(item);
+  });
+})();
+
+function render(item) {
+  return `<tr><td><strong>${item.bookId}</strong></td><td>${item.isbn}</td><td>${item.title}</td><td>${item.overdueFee}</td><td>${item.publisher}</td><td>${item.datePublished}</td></tr>`;
+}
